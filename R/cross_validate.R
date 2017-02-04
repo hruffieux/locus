0d1f37833d4a330e70ab441cabc1f3d88c794b04
Helene Ruffieux <helene.ruffieux@epfl.ch>
2017-02-04 15:05:56 +0100

    Moved to version 0.2.0.

--- R/cross_validate.R
+++ R/cross_validate.R
@@ -181,6 +181,7 @@ cross_validate_ <- function(Y, X, Z, list_cv, user_seed, verbose) {
       }
 
       Y_tr <- scale(Y_tr, center = TRUE, scale = FALSE)
+
       #if (family == "gaussian") Y_test <- scale(Y_test, center = TRUE, scale = FALSE)
       Y_test <- scale(Y_test, center = TRUE, scale = FALSE)
 
@@ -238,7 +239,7 @@ cross_validate_ <- function(Y, X, Z, list_cv, user_seed, verbose) {
                              xi, zeta2_inv_vb, m2_alpha, m1_beta, m2_beta, sum_gam)
             })
           }
-        # } else {
+        # } else { # issue: chi_vb would need to be restriced to the test set.
         #
         #   vb_tr <- locus_bin_core_(Y_tr, X_tr, list_hyper_pg,
         #                            list_init_pg$chi_vb, list_init_pg$gam_vb,
@@ -246,15 +247,16 @@ cross_validate_ <- function(Y, X, Z, list_cv, user_seed, verbose) {
         #                            list_init_pg$sig2_beta_vb, tol_cv, maxit_cv,
         #                            batch_cv, verbose, full_output = TRUE)
         #
-        # # issue: chi_vb would need to be restriced to the test set.
+        #
         #   lb_vec[ind_pg] <- with(vb_tr, {
-        #     lower_bound_bin_(Y_test, X_test, Z_test, a, a_vb, b, b_vb, chi_vb, gam_vb,
-        #                      lambda, nu, phi, phi_vb, psi_vb, sig2_alpha_vb,
-        #                      sig2_beta_vb, sig2_inv_vb, xi, zeta2_inv_vb,
-        #                      mu_alpha, m1_beta, m2_alpha, m2_beta, mat_x_m1,
-        #                      mat_z_mu)
+        #     lower_bound_bin_(Y_test, X_test, Z_test, a, a_vb, b, b_vb, chi_vb,
+        #                      gam_vb, lambda, nu, phi, phi_vb, psi_vb,
+        #                      sig2_alpha_vb, sig2_beta_vb, sig2_inv_vb, xi,
+        #                      zeta2_inv_vb, mu_alpha, m1_beta, m2_alpha,
+        #                      m2_beta, mat_x_m1, mat_z_mu)
         #   })
         # }
+
         if (verbose) { cat(paste("Lower bound on test set, fold ", k, ", p0_av ",
                                  pg, ": ", lb_vec[ind_pg], ". \n", sep = ""))
           cat("-------------------------\n") }
